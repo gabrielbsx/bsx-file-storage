b3a2eec7e433577d02975dd1907ba9051b1c737a
Gabriel Barbosa <gabrielturing@gmail.com>
2023-03-26 07:31:22 -0300

    feat: add SchemaValidator

--- src/main/describe/index.ts
+++ src/main/describe/index.ts
@@ -1,10 +1,16 @@
+export interface SchemaValidator {
+  type: 'string' | 'number' | 'integer' | 'boolean' | 'array' | 'object'
+  properties?: Record<string, SchemaValidator> | SchemaValidator
+  items?: SchemaValidator
+}
+
 export interface RouteDescribe {
   url: string
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD' | 'OPTIONS'
   schema: {
-    body?: any
-    querystring?: any
-    params?: any
+    body?: SchemaValidator
+    querystring?: SchemaValidator
+    params?: SchemaValidator
   }
   handler: (request: any, reply: any) => Promise<any>
 }
